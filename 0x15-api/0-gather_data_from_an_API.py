769d790458a114fe2b8accb712a3dbb4aa3fd3fa
Toluthedev <enochjesse884@gmail.com>
2023-05-22 21:14:42 +0000

    changed

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,36 +1,35 @@
 #!/usr/bin/python3
+import requests
+import sys
 
-"""
-Python script that, using a REST API, for a given employee ID,
-returns information about his/her TODO list progress.
-"""
+def get_employee_todo_progress(employee_id):
+    # Fetch employee data from the API
+    response = requests.get(f"https://jsonplaceholder.typicode.com/todos/{employee_id}")
+    
+    if response.status_code == 200:
+        employee_data = response.json()
+        
+        # Get the employee's name
+        employee_name = employee_data['name']
+        
+        # Get the total number of tasks
+        total_tasks = len(employee_data['todos'])
+        
+        # Get the number of completed tasks and their titles
+        completed_tasks = [task['title'] for task in employee_data['todos'] if task['completed']]
+        num_completed_tasks = len(completed_tasks)
+        
+        # Print the progress information
+        print(f"Employee {employee_name} is done with tasks ({num_completed_tasks}/{total_tasks}):")
+        for task_title in completed_tasks:
+            print("\t", task_title)
+    else:
+        print(f"Failed to retrieve employee data. Error: {response.status_code}")
 
-from requests import get
-from sys import argv
-
-
-if __name__ == "__main__":
-    response = get('https://jsonplaceholder.typicode.com/todos/')
-    data = response.json()
-    completed = 0
-    total = 0
-    tasks = []
-    response2 = get('https://jsonplaceholder.typicode.com/users')
-    data2 = response2.json()
-
-    for i in data2:
-        if i.get('id') == int(argv[1]):
-            employee = i.get('name')
-
-    for i in data:
-        if i.get('userId') == int(argv[1]):
-            total += 1
-
-            if i.get('completed') is True:
-                completed += 1
-                tasks.append(i.get('title'))
-
-    print("Employee {} is done with tasks({}/{}):".format(employee, completed,total))
-
-    for i in tasks:
-        print("\t {}".format(i))
+if __name__ == '__main__':
+    # Check if an employee ID is provided as a command-line argument
+    if len(sys.argv) == 2:
+        employee_id = int(sys.argv[1])
+        get_employee_todo_progress(employee_id)
+    else:
+        print("Please provide the employee ID as a command-line argument.")
