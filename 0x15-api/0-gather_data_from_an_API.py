5fd8fc39bec103c79863671ee9b9c760310e3bbf
Toluthedev <enochjesse884@gmail.com>
2023-05-22 21:20:48 +0000

    changed

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,35 +1,52 @@
 #!/usr/bin/python3
+"""Returns to-do list information for a given employee ID."""
+
 import requests
 import sys
 
-def get_employee_todo_progress(employee_id):
-    # Fetch employee data from the API
-    response = requests.get(f"https://jsonplaceholder.typicode.com/todos/{employee_id}")
-    
-    if response.status_code == 200:
-        employee_data = response.json()
-        
-        # Get the employee's name
-        employee_name = employee_data['name']
-        
-        # Get the total number of tasks
-        total_tasks = len(employee_data['todos'])
-        
-        # Get the number of completed tasks and their titles
-        completed_tasks = [task['title'] for task in employee_data['todos'] if task['completed']]
-        num_completed_tasks = len(completed_tasks)
-        
-        # Print the progress information
-        print(f"Employee {employee_name} is done with tasks ({num_completed_tasks}/{total_tasks}):")
-        for task_title in completed_tasks:
-            print("\t", task_title)
-    else:
-        print(f"Failed to retrieve employee data. Error: {response.status_code}")
 
-if __name__ == '__main__':
-    # Check if an employee ID is provided as a command-line argument
-    if len(sys.argv) == 2:
-        employee_id = int(sys.argv[1])
-        get_employee_todo_progress(employee_id)
+def get_user_info(employee_id):
+    """Returns the user information for the given employee ID."""
+    url = "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()
     else:
-        print("Please provide the employee ID as a command-line argument.")
+        raise Exception("Could not get user information for employee ID {}".format(employee_id))
+
+
+def get_todo_list(employee_id):
+    """Returns the to-do list for the given employee ID."""
+    url = "https://jsonplaceholder.typicode.com/todos"
+    params = {"userId": employee_id}
+    response = requests.get(url, params=params)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        raise Exception("Could not get to-do list for employee ID {}".format(employee_id))
+
+
+def get_completed_tasks(todo_list):
+    """Returns a list of completed tasks from the given to-do list."""
+    completed_tasks = []
+    for task in todo_list:
+        if task["completed"]:
+            completed_tasks.append(task)
+    return completed_tasks
+
+
+def main():
+    """The main function."""
+    employee_id = sys.argv[1]
+    user_info = get_user_info(employee_id)
+    todo_list = get_todo_list(employee_id)
+    completed_tasks = get_completed_tasks(todo_list)
+
+    print("Employee {} is done with tasks({}/{}):".format(
+        user_info["name"], len(completed_tasks), len(todo_list)))
+    for task in completed_tasks:
+        print("\t {}".format(task["title"]))
+
+
+if __name__ == "__main__":
+    main()
